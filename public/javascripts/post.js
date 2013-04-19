c48a19dfc5cd295f468a90b67c38fbda381bd777
Chris Roth <chris@cjroth.com>
2013-04-18 22:04:31 -0600

    design work on editing posts

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -14,16 +14,38 @@
     e.stopPropagation();
     $('.post').addClass('edit'); 
     $('.post-title').slideUp(30);
-    $(document).bind('click', function(e) {
-      var markdown = $('.edit-content').val();
-      var html = marked(markdown);
-      $('.post-content').html(html);
-      $('.post').removeClass('edit');
-      $('.post-title').slideDown(30);
+    $('.post-options-handle').position({
+      my: 'right center',
+      at: 'right center',
+      of: '.edit-content'
     });
-    $('.edit-content').bind('click', function(e) {
-      e.stopPropagation();
+
+  });
+
+  $(document).bind('click', function(e) {
+    var markdown = $('.edit-content').val();
+    var html = marked(markdown);
+    $('.post-content').html(html);
+    $('.post').removeClass('edit');
+    $('.post-title').slideDown(30);
+  });
+
+  $('.edit-content').bind('click', function(e) {
+    e.stopPropagation();
+  });
+
+  $('.post-options-handle').click(function(e) {
+    e.stopPropagation();
+    $('.edit-container').toggleClass('options');
+    $(this).position({
+      my: 'right center',
+      at: 'right center',
+      of: '.edit-content'
     });
+  });
+
+  $('.post-options').click(function(e) {
+    e.stopPropagation();
   });
 
 })();
